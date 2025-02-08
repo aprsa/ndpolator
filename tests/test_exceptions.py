4371bbbdbbed0e56c649b14a03ca04c3e470a83a
Andrej Prsa <aprsa09@gmail.com>
2025-02-08 17:16:20 -0500

    Corrects argument name in tests

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -25,15 +25,15 @@ def test_wrapper_typing():
     axes = (ax, ax)
     ndp = ndpolator.Ndpolator(axes)
     
-    table = 123
+    name = 123
     grid = 123
     with pytest.raises(TypeError):
-        ndp.register(table=table, associated_axes=None, grid=grid)
+        ndp.register(name=name, associated_axes=None, grid=grid)
 
-    table = 'test'
+    name = 'test'
     grid = 123
     with pytest.raises(TypeError):
-        ndp.register(table=table, associated_axes=None, grid=grid)
+        ndp.register(name=name, associated_axes=None, grid=grid)
 
     grid = np.random.uniform(low=0, high=1, size=(3, 3, 1))
-    ndp.register(table=table, associated_axes=None, grid=grid)
+    ndp.register(name=name, associated_axes=None, grid=grid)
