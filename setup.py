b46238cab9deceab271763fdad5188542e7b9fe6
Andrej Prsa <andrej@prsa.us>
2026-08-06 12:35:02 -0400

    adds support for MSVC flags

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
 import numpy
+
+
+class BuildExtFlags(build_ext):
+    """Custom build_ext command to add compiler-specific options."""
+
+    def build_extensions(self):
+        # we need to fiddle with compiler flags based on the compiler used (gcc, clang, msvc)
+        compiler_type = self.compiler.compiler_type
+        for ext in self.extensions:
+            if compiler_type == 'msvc':
+                ext.extra_compile_args = ['/O2', '/WX']
+                ext.extra_link_args = []
+            else:
+                ext.extra_compile_args = ['-O3', '-Wall', '-Wextra', '-Werror']
+                ext.extra_link_args = ['-lm', '-pthread']
+        super().build_extensions()
+
 
 ext_modules = [
     Extension(
@@ -13,13 +31,11 @@ ext_modules = [
             'external/kdtree/kdtree.c',
         ],
         language='c',
-        extra_link_args=['-lm', '-pthread'],
-        extra_compile_args=["-Werror", "-O3"],
-        # extra_compile_args=["-Werror", "-O0", "-g"],
         include_dirs=['src', 'external/kdtree', numpy.get_include()],
     ),
 ]
 
 setup(
+    cmdclass={'build_ext': BuildExtFlags},
     ext_modules=ext_modules,
 )
